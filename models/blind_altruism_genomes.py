7009c748455e125a9f1c73670afa9ea89883f0e8
Noah Jornet <noahjorcal@gmail.com>
2023-05-26 11:54:04 +0200

    blind_altruism_genomes.py and relatedness.py documented

--- models/blind_altruism_genomes.py
+++ models/blind_altruism_genomes.py
@@ -4,17 +4,17 @@ import random
 from configparser import ConfigParser
 import sys
 from os import path
-import os
-import time
+from relatedness import relatedness_coefficient
+
 # Add the alts directory to the Python path
 sys.path.append(path.join(path.dirname(__file__), '..'))
 
-from relatedness import relatedness_coefficient
-
+# Import general configuration
 general_config = ConfigParser()
 config_path = path.join(path.dirname(__file__), '..', 'config.ini')
 general_config.read(config_path)
 
+# Save altruism configuration
 exp_factor_config = float(general_config['population']['benefit_relatedness_exp_factor'])
 cost_benefit_ratio_config = float(general_config['population']['cost_benefit_ratio'])
 minimum_benefit_config = float(general_config['population']['minimum_benefit'])
@@ -22,6 +22,17 @@ maximum_cost_config = float(general_config['population']['maximum_cost'])
 
 
 def exp_f(x, factor, penetrance, min_benefit):
+    """
+    Exponential distribution for calculating benefit (y) based on relatedness (x)
+    :param float x: Relatedness
+    :param float factor: Factor that determines the slope of the exponential distribution. The higher the value,
+    the steeper slope.
+    :param float penetrance: Effect of altruism.
+    In homozygous individuals the penetrance is 1 and in heterozygous it is 0.5.
+    :param float min_benefit: Minimum benefit altruists will give regardless of the relatedness.
+    :return: Benefit for the recipient based on the relatedness.
+    """
+    # When the factor is 0, the distribution is lineal
     if factor == 0:
         if x * penetrance < min_benefit:
             return min_benefit
@@ -30,9 +41,10 @@ def exp_f(x, factor, penetrance, min_benefit):
     y = exp(factor * x) - 1
     minimum = exp(factor * 0) - 1
     maximum = exp(factor * 1) - 1
+    # Distribution scaled between 0 and 1
     y = (y - minimum) / (maximum - minimum) * penetrance
-    ''' If the calculated benefit from the relatedness is less than the minimum benefit,
-    the later will be the final benefit '''
+    # If the calculated benefit from the relatedness is less than the minimum benefit,
+    # the later will be the final benefit
     if y < min_benefit:
         return min_benefit
     else:
@@ -40,6 +52,12 @@ def exp_f(x, factor, penetrance, min_benefit):
 
 
 def get_possible_recipients(altruist, group):
+    """
+    Discards incompatible recipients for altruism. In this model the only one is the altruist itself.
+    :param simulator.Individual altruist: Altruistic individual that will be deleted from the group.
+    :param list[simulator.Individual] group: Groups where the altruist belongs to
+    :return: Groups without the incompatible recipients.
+    """
     possible_recipients = group.copy()
     possible_recipients.pop(group.index(altruist))
     return possible_recipients
@@ -47,12 +65,31 @@ def get_possible_recipients(altruist, group):
 
 def altruistic_act(altruist, possible_recipients, penetrance, added_cost,
                    exp_factor, cost_benefit_ratio, minimum_benefit, maximum_cost):
-    # The benefit is multiplied by the penetrance, if there is no codominance the penetrance is 1, if there is it if 0.5
+    """
+    The altruistic individual gives part of its survival probability to a random individual from the possible recipients
+    based their relatedness and an exponential distribution.
+    :param simulator.Individual altruist: Altruistic individual that will give part of its survival probability.
+    :param list[simulator.Individual] possible_recipients: Altruist's group without it from which the recipient will
+    be selected
+    :param float penetrance: Benefit based on relatedness, minimum benefit and maximum cost is based on penetrance.
+    In homozygous individuals the penetrance is 1 and in heterozygous it is 0.5.
+    :param float added_cost: Added survival probability sacrificed by the current altruist.
+    :param float exp_factor: Factor that determines the slope of the exponential distribution. The higher the value,
+    the steeper slope.
+    :param float cost_benefit_ratio: Cost = Benefit * Ratio
+    :param float minimum_benefit: Minimum benefit altruists will give regardless of the relatedness.
+    :param float maximum_cost: When the added cost reaches the maximum cost the benefit in the interaction will be the
+    remaining cost to reach the maximum.
+    :return: The new modified added cost.
+    """
+    # The benefit is multiplied by the penetrance
+    # If there is no codominance the penetrance is 1, if there is, it is 0.5
     minimum_benefit = minimum_benefit * penetrance
     maximum_cost = maximum_cost * penetrance
-    # A random individual from the group is selected
+    # A random individual from the group is selected and deleted from the group
     random.shuffle(possible_recipients)
     recipient = possible_recipients.pop()
+    # The altruist will only help the selected individual if it has equal or less survival probability
     if recipient.survival_probability <= altruist.survival_probability:
         # The ancestry list is prepared for the calculation of the relatedness
         recipient_ancestry = [[recipient.id]]
@@ -67,11 +104,12 @@ def altruistic_act(altruist, possible_recipients, penetrance, added_cost,
             benefit = exp_f(relatedness_coef, exp_factor, penetrance, minimum_benefit)
         # The cost is calculated from the benefit and the survival probabilities are recalculated
         cost = benefit * cost_benefit_ratio
-        ''' If the cost surpass the maximum cost the altruist will sacrifice up to the maximum,
-        instead of based on the relatedness '''
+        # If the cost exceeds the maximum cost the altruist will sacrifice up to the maximum,
+        # instead of based on the relatedness
         if maximum_cost - added_cost < cost:
             cost = maximum_cost - added_cost
             benefit = cost / cost_benefit_ratio
+        # The survival probabilities are modified
         recipient.survival_probability += benefit
         altruist.survival_probability -= cost
         return added_cost + cost
@@ -82,33 +120,43 @@ def altruistic_act(altruist, possible_recipients, penetrance, added_cost,
 def selection(groups,
               exp_factor=exp_factor_config, cost_benefit_ratio=cost_benefit_ratio_config,
               minimum_benefit=minimum_benefit_config, maximum_cost=maximum_cost_config):
-
-    if exp_factor != 0 or minimum_benefit != 0:
-        for group in groups:
-            for individual in group:
-                if individual.phenotype[0] == 'altruistic':
-                    possible_recipients = get_possible_recipients(individual, group)
-                    if len(possible_recipients) != 0:
-                        added_cost = 0
-                        while maximum_cost > added_cost:
-                            ''' If the new added cost ends up being higher than the maximum cost,
-                            the last interaction will not be reverted, so in some cases,
-                            the individual will have sacrificed more fitness than expected'''
-                            added_cost = altruistic_act(individual, possible_recipients, 1, added_cost,
-                                                        exp_factor, cost_benefit_ratio,
-                                                        minimum_benefit, maximum_cost)
-                            if len(possible_recipients) == 0:
-                                break
-                elif '_' in individual.phenotype[0]:
-                    possible_recipients = get_possible_recipients(individual, group)
-                    if len(possible_recipients) != 0:
-                        added_cost = 0
-                        while (maximum_cost * 0.5) > added_cost:
-                            ''' If the new added cost ends up being higher than the maximum cost,
-                            the last interaction will not be reverted, so in some cases,
-                            the individual will have sacrificed more fitness than expected'''
-                            added_cost = altruistic_act(individual, possible_recipients, 0.5, added_cost,
-                                                        exp_factor, cost_benefit_ratio,
-                                                        minimum_benefit, maximum_cost)
-                            if len(possible_recipients) == 0:
-                                break
+    """
+    For each individual in the group, if it is altruistic, it will help other individuals.
+    :param list[list[simulator.Individual]] groups: Whole population structured in groups.
+    :param float exp_factor: Factor that determines the slope of the exponential distribution. The higher the value,
+    the steeper slope.
+    :param float cost_benefit_ratio: Cost = Benefit * Ratio
+    :param float minimum_benefit: Minimum benefit altruists will give regardless of the relatedness.
+    :param float maximum_cost: When the added cost reaches the maximum cost the benefit in the interaction will be the
+    remaining cost to reach the maximum.
+    """
+    for group in groups:
+        for individual in group:
+            # If the individual is altruistic homozygous
+            if individual.phenotype[0] == 'altruistic':
+                possible_recipients = get_possible_recipients(individual, group)
+                if len(possible_recipients) != 0:
+                    added_cost = 0
+                    while maximum_cost > added_cost:
+                        # If the new added cost ends up being higher than the maximum cost,
+                        # the last interaction will not be reverted, so in some cases,
+                        # the individual will have sacrificed more fitness than expected
+                        added_cost = altruistic_act(individual, possible_recipients, 1, added_cost,
+                                                    exp_factor, cost_benefit_ratio,
+                                                    minimum_benefit, maximum_cost)
+                        if len(possible_recipients) == 0:
+                            break
+            # If the individual is altruistic heterozygous
+            elif '_' in individual.phenotype[0]:
+                possible_recipients = get_possible_recipients(individual, group)
+                if len(possible_recipients) != 0:
+                    added_cost = 0
+                    while (maximum_cost * 0.5) > added_cost:
+                        # If the new added cost ends up being higher than the maximum cost,
+                        # the last interaction will not be reverted, so in some cases,
+                        # the individual will have sacrificed more fitness than expected
+                        added_cost = altruistic_act(individual, possible_recipients, 0.5, added_cost,
+                                                    exp_factor, cost_benefit_ratio,
+                                                    minimum_benefit, maximum_cost)
+                        if len(possible_recipients) == 0:
+                            break
--- relatedness.py
+++ relatedness.py
@@ -1,48 +1,58 @@
 def relatedness_coefficient(ancestry_1, ancestry_2):
+    """
+    Calculated relatedness from the ancestry of two individuals.
+    :param list[list[int]] ancestry_1: IDs of the ancestors of an individual. Each list within contains the IDs of the
+    individuals of one tree's generation.
+    :param list[list[int]] ancestry_2: IDs of the ancestors of an individual. Each list within contains the IDs of the
+    individuals of one tree's generation.
+    :return: Relatedness coefficient between the two individuals.
+    """
     relatedness = 0
+
     indexes_to_skip = {0: {0: [], 1: [], 2: [], 3: []},
                        1: {0: [], 1: [], 2: [], 3: []},
                        2: {0: [], 1: [], 2: [], 3: []},
                        3: {0: [], 1: [], 2: [], 3: []}}
+    # For generation in the genealogy
     for gen_1_index in range(len(ancestry_1)):
         for gen_2_index in range(len(ancestry_2)):
+            # For ancestor in the generation
             for ind_1_index in range(len(ancestry_1[gen_1_index])):
                 for ind_2_index in range(len(ancestry_2[gen_2_index])):
-                    # temp1 = ancestry_1[gen_1_index][ind_1_index]
-                    # temp2 = ancestry_2[gen_2_index][ind_2_index]
-                    # temp1000 = indexes_to_skip[gen_1_index]
-                    # iff = (ind_1_index, ind_2_index)
-                    # inn = indexes_to_skip[gen_1_index][gen_2_index]
+                    # If the ancestor is not unknown, it matches any other ancestor from the other ancestry and was not
+                    # already considered by a closer ancestor
                     if ancestry_1[gen_1_index][ind_1_index] != 0 and \
                             ancestry_1[gen_1_index][ind_1_index] == ancestry_2[gen_2_index][ind_2_index] and \
                             (ind_1_index, ind_2_index) not in indexes_to_skip[gen_1_index][gen_2_index]:
+                        # Generations between the common ancestor to both individuals
                         edges = gen_1_index + gen_2_index
                         relatedness += (1 / 2) ** edges
                         younger_gen_index = min(gen_1_index, gen_2_index)
                         gen_difference = abs(gen_1_index - gen_2_index)
+                        # Adds indices to skip
                         for gen_i in range(younger_gen_index + 1, len(ancestry_1) - gen_difference):
                             if gen_1_index == younger_gen_index:
-                                from_index_1 = int(ind_1_index * (len(ancestry_1[gen_i]) / len(ancestry_1[gen_1_index])))
-                                to_index_1 = int(from_index_1 + (1 / len(ancestry_1[gen_1_index])) * len(ancestry_1[gen_i]))
-                                from_index_2 = int(ind_2_index * (len(ancestry_2[gen_i + gen_difference]) / len(ancestry_2[gen_2_index])))
-                                to_index_2 = int(from_index_2 + (1 / len(ancestry_2[gen_2_index])) * len(ancestry_2[gen_i + gen_difference]))
+                                from_index_1 = int(ind_1_index * (len(ancestry_1[gen_i]) /
+                                                                  len(ancestry_1[gen_1_index])))
+                                to_index_1 = int(from_index_1 + (1 / len(ancestry_1[gen_1_index]))
+                                                 * len(ancestry_1[gen_i]))
+                                from_index_2 = int(ind_2_index * (len(ancestry_2[gen_i + gen_difference]) /
+                                                                  len(ancestry_2[gen_2_index])))
+                                to_index_2 = int(from_index_2 + (1 / len(ancestry_2[gen_2_index])) *
+                                                 len(ancestry_2[gen_i + gen_difference]))
                                 indexes_to_skip[gen_i][gen_i + gen_difference].extend(
                                     list(zip(range(from_index_1, to_index_1),
                                              range(from_index_2, to_index_2))))
                             else:
-                                from_index_1 = int(ind_1_index * (len(ancestry_1[gen_i + gen_difference]) / len(ancestry_1[gen_1_index])))
-                                to_index_1 = int(from_index_1 + (1 / len(ancestry_1[gen_1_index])) * len(ancestry_1[gen_i + gen_difference]))
-                                from_index_2 = int(ind_2_index * (len(ancestry_2[gen_i]) / len(ancestry_2[gen_2_index])))
-                                to_index_2 = int(from_index_2 + (1 / len(ancestry_2[gen_2_index])) * len(ancestry_2[gen_i]))
+                                from_index_1 = int(ind_1_index * (len(ancestry_1[gen_i + gen_difference]) /
+                                                                  len(ancestry_1[gen_1_index])))
+                                to_index_1 = int(from_index_1 + (1 / len(ancestry_1[gen_1_index])) *
+                                                 len(ancestry_1[gen_i + gen_difference]))
+                                from_index_2 = int(ind_2_index * (len(ancestry_2[gen_i]) /
+                                                                  len(ancestry_2[gen_2_index])))
+                                to_index_2 = int(from_index_2 + (1 / len(ancestry_2[gen_2_index])) *
+                                                 len(ancestry_2[gen_i]))
                                 indexes_to_skip[gen_i + gen_difference][gen_i].extend(
                                     list(zip(range(from_index_1, to_index_1),
                                              range(from_index_2, to_index_2))))
     return relatedness
-
-
-# print(relatedness_coefficient([[14], [10, 7], [0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]],
-#                               [[24], [10, 14], [0, 0, 10, 7], [0, 0, 0, 0, 0, 0, 0, 0]]))
-
-# relatedness_coefficient([[2000], [1000, 1], [1, 2, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]],
-#                         [[1000], [1, 2], [0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]])
-
